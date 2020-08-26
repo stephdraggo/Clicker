45bdcf42f26ba09aaa40919b7137a6abea0c4f53
stephdraggo <stephdraggo@users.noreply.github.com>
2020-08-26 13:00:13 +1000

    yee class over

---
+++ Assets/Scripts/Calculations.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace my
+{
+    public class Calculations : MonoBehaviour
+    {
+        #region Variables
+        [Header("Reference Variables")]
+        public float availableFunds;
+        #endregion
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
